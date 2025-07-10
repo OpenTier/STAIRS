ad96c3c1ffc3de1c6b81a7c7ba3f8eb2439b3f7a
efraim nabil <efraimefefraim@gmail.com>
2025-07-10 13:51:47 +0300

    refactor: remove basic API test implementation

--- tests/api/basic.test.js
+++
@@ -1,8 +0,0 @@
-// @ts-check
-import { test, expect } from '@playwright/test';
-
-test('basic test', async ({ request }) => {
-  const response = await request.get('http://localhost:3001/devices');
-  console.log('Response status:', response.status());
-  expect(response.status()).toBe(200);
-});
