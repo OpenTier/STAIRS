ae53ae4bdd0d20261b00563ed20720e563cc10ca
efraim nabil <efraimefefraim@gmail.com>
2025-07-15 17:39:03 +0300

    Fix formatting

--- cloud/backend/device_gateway/device_gateway/twin/vehicle_state.py
+++ cloud/backend/device_gateway/device_gateway/twin/vehicle_state.py
@@ -17,6 +17,7 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with this file.  If not, see <https://www.gnu.org/licenses/>.
 
+
 class VehicleState:
     def __init__(
         self,
