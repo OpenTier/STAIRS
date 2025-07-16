ee3a28a9b5955b8c9c8924290495e0a80f1d7682
efraim nabil <efraimefefraim@gmail.com>
2025-07-16 14:38:24 +0300

    Revert "Add LGPL-3.0 license headers to all files"
    This reverts commit f0266d67e53418dc45b87c909ba28ae4508f1319.

--- cloud/backend/device_gateway/device_gateway/twin/vehicle_state.py
+++ cloud/backend/device_gateway/device_gateway/twin/vehicle_state.py
@@ -1,22 +1,3 @@
-# Copyright (c) 2025 by OpenTier GmbH
-# SPDX-FileCopyrightText: 2025 OpenTier GmbH
-# SPDX-License-Identifier: LGPL-3.0-or-later
-
-# This file is part of OpenTier.
-
-# This is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as
-# published by the Free Software Foundation; either version 3 of the
-# License, or (at your option) any later version.
-
-# This is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Lesser General Public License for more details.
-
-# You should have received a copy of the GNU Lesser General Public
-# License along with this file.  If not, see <https://www.gnu.org/licenses/>.
-
 class VehicleState:
     def __init__(
         self,
